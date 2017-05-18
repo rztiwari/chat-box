9801c376872a178ce557a00513771120e8523f4f
Tiwari <rajiv.tiwari@capgemini.com>
2017-05-18 23:31:54 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('user joined', function(user){
+    io.emit('user joined', user);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
