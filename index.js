a5185da881ad7d117e791f386330d6bdb5220afe
Tiwari <rajiv.tiwari@capgemini.com>
2017-05-19 09:58:49 +0100

    Some styling changes

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+// All imports
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// PORT
 var PORT = process.env.PORT || 3001;
 
+// Static file serving
 app.use(express.static('public'));
 
+// Base/ index routing
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// Socket connection and chat logic
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
@@ -20,6 +25,7 @@ io.on('connection', function(socket){
   });
 });
 
+// http port listning
 http.listen(PORT, function(){
   console.log('listening on port ' + PORT);
 });
